25d96d6ffe72eb80dfbdd640cd6b1c71d19316bb
Denis Lepekhin <denis.lepekhin@gmail.com>
2018-08-22 15:07:04 +0300

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ pages=../dlepex-github-io
 dest=$pages/7w
 when=`date +%Y-%m-%d.%H:%M`
 
-cp ./**/*.html ./**/*.js ./**/*.css $dest
+cp *.html *.js *.css $dest
 cd $pages
 git  add .
 git  commit -m "deploy 7w at $when"
