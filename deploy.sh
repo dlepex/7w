ab335bb3ba42b7d6d8c945f86e61891864244ace
Denis Lepekhin <denis.lepekhin@gmail.com>
2018-08-22 15:05:57 +0300

    meta+, deploy script, css fix

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 
 pages=../dlepex-github-io
 dest=$pages/7w
+when=`date +%Y-%m-%d.%H:%M`
 
 cp ./**/*.html ./**/*.js ./**/*.css $dest
-cd @pages
-
+cd $pages
 git  add .
-git commit -m "deploy 7w"
+git  commit -m "deploy 7w at $when"
 git  push
